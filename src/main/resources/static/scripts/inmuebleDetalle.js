03913cef27e54ebb2de40103b7cc4db4f8a9f517
Arturo Gonzales Rodriguez <adgr_10@hotmail.com>
2023-12-14 13:20:37 -0300

    Detalle Inmueble con btns

--- src/main/resources/static/scripts/inmuebleDetalle.js
+++ src/main/resources/static/scripts/inmuebleDetalle.js
@@ -1,8 +1,7 @@
 function mostrarImagen(imagenClickeada) {
-// Obtener la ruta de la imagen clickeada
-        var rutaImagen = imagenClickeada.src;
-        
-        // Actualizar la imagen en la fila-1
-        document.getElementById("imagenGrande").src = rutaImagen;
-    
+  // Obtener la ruta de la imagen clickeada
+  var rutaImagen = imagenClickeada.src;
+
+  // Actualizar la imagen en la fila-1
+  document.getElementById("imagenGrande").src = rutaImagen;
 }
