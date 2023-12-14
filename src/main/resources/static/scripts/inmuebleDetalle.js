22fe522c54d56fd54df553b005aa270955b7f36a
Arturo Gonzales Rodriguez <adgr_10@hotmail.com>
2023-12-14 15:19:56 -0300

    inmueble detalle contacto funcional

--- src/main/resources/static/scripts/inmuebleDetalle.js
+++ src/main/resources/static/scripts/inmuebleDetalle.js
@@ -5,3 +5,24 @@ function mostrarImagen(imagenClickeada) {
   // Actualizar la imagen en la fila-1
   document.getElementById("imagenGrande").src = rutaImagen;
 }
+
+const btn = document.getElementById('button');
+
+document.getElementById('form')
+ .addEventListener('submit', function(event) {
+   event.preventDefault();
+
+   btn.value = 'Enviando...';
+
+   const serviceID = 'MrHouseOutlook';
+   const templateID = 'MrHouseTemplate1';
+
+   emailjs.sendForm(serviceID, templateID, this)
+    .then(() => {
+      btn.value = 'Enviar Consulta';
+      alert('Email enviado!');
+    }, (err) => {
+      btn.value = 'Enviar Consulta';
+      alert(JSON.stringify(err));
+    });
+});
